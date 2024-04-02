87ded545e19804ff273a5cd0b964db4fdb8634cd
Trump <60252035+Water-blue7@users.noreply.github.com>
2024-04-02 21:03:18 +0800

    重命名：文件批量生成、文件批量重命名

--- 12-重命名.js
+++ 12-重命名.js
@@ -3,10 +3,11 @@ const { log } = require('console');
 const fs = require('fs');
 
 var dir = 'dir';
+var newDir = 'newDir'
 
 // 2. 创建一些文件
 
-fs.stat('./' + dir, (err, data) => {
+fs.stat(dir, (err, data) => {
     if (err) {
         log('ERROR: ' + err);
         log('操作失败==>不存在此文件夹或……');
@@ -14,7 +15,7 @@ fs.stat('./' + dir, (err, data) => {
         return;
     } else {
         if (data.isDirectory) {
-            fs.rm('./' + dir, { recursive: true }, err => {
+            fs.rm(dir, { recursive: true }, err => {
                 if (err) {
                     log('删除失败');
                     return;
@@ -29,7 +30,7 @@ fs.stat('./' + dir, (err, data) => {
     }
 });
 function makeDir(dir) {
-    fs.mkdir('./' + dir, err => {
+    fs.mkdir(dir, err => {
         if (err) {
             log('创建失败');
             return;
@@ -37,20 +38,30 @@ function makeDir(dir) {
             log('创建成功');
             for (let i = 0; i <= 15; i++) {
                 log(i);
-                writeFile(i + '.txt');
+                writeFile(i + '-文本.txt');
             }
-            const files = fs.readdirSync('./dir');
+            const files = fs.readdirSync(dir);
             // log(files);
 
-            files.forEach(item=>{
-                log(item);
+            files.forEach(item => {
+                // split()  将字符串分割成数组
+                let data = item.split('-');
+                let [num, name] = data;
+                if (Number(num) < 10) {
+                    num = '0' + num;
+                }
+                // 创建新的文件名
+                let newName = num + '-' + name;
+                // 重命名
+                fs.renameSync(dir + `/${item}`, dir + `/${newName}`);
+                log(newName);
             });
         }
     });
 }
 function writeFile(fileName) {
     const fs1 = require('fs');
-    fs1.writeFile('./dir/' + fileName, fileName + 'haha', err => {
+    fs1.writeFile(dir + '/' + fileName, fileName + 'haha', err => {
         if (err) {
             console.error('Error :', err);
             log('写入失败');

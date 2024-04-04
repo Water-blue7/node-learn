1830cdb1fed7051367eb9a9f394220200c9a51d0
Trump <Trump@gmail.com>
2024-04-05 01:48:05 +0800

    获取路由参数

---
+++ express-learn/04_获取路由参数.js
@@ -0,0 +1,14 @@
+// 导入express
+const express = require('express');
+// 创建express应用对象
+const app = express();
+// 创建路由
+app.get('/:id.html', (req, res) => {
+    // 获取URL路由参数
+    console.log(req.params.id);
+    res.end('hello express');
+});
+// 监听端口，启动服务
+app.listen(3000, () => {
+    console.log('服务已启动，端口：3000......');
+});

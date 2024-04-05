d1e8cf78a632820562d0c81574e364516f88f2a6
Trump <Trump@gmail.com>
2024-04-06 00:42:54 +0800

    文件上传

--- 15-generator/routes/index.js
+++ 15-generator/routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+// 导入formidable
+const formidable = require('formidable');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 显示网页的表单
+router.get('/portrait', (req, res) => {
+  res.render('portrait');
+});
+
+// 处理文件上传
+router.post('/portrait', (req, res) => {
+  // 创建form对象
+  const form = formidable({
+    multiples: true,
+    // 设置上传文件的保存目录
+    uploadDir: __dirname + '/../public/images',
+    // 保持文件后缀
+    keepExtensions: true
+  });
+  // 解析请求
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      next(err);
+      return;
+    }
+    // console.log(fields);
+    // console.log(files);
+
+    // 服务器中图片地址
+    let url='/images/'+files.portrait.newFilename;
+    res.send(url);
+  });
+});
 module.exports = router;
